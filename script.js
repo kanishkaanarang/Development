469bb15da29afa5290fd40747bea9d7a8915e4c2
Kanishka Narang <kanishkaanarang@gmail.com>
2025-02-01 17:31:01 +0530

    Add files via upload
    first basic JS project: number guessing game .

---
+++ script.js
@@ -0,0 +1,22 @@
+/*let a=prompt("enter a number");
+if(a%5===0){
+    console.log("multiple of 5");
+}
+else{
+    console.log("not a multiple of 5");
+}*/
+
+/*for(let i=0;i<5;i++){
+    console.log("kanishka");
+}*/
+
+let a="25";
+
+let x=prompt("enter a number");
+while(x!=a){
+    x=prompt("wrong number, enter again");
+
+
+}
+
+console.log("yay, you won!")
